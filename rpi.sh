8a194ac43db93ca225818016da945081e54be353
gobikrishnan-s3141 <139465475+gobikrishnan-s3141@users.noreply.github.com>
2024-12-09 22:35:57 +0000

    Add files via upload

---
+++ rpi.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# install packages
+sudo apt update
+sudo apt install build-essential bash-completion htop neofetch iftop udiskie rsync ssh tree git python3-dev cmake libssl-dev libxml2-dev libcurl4-openssl-dev libfontconfig-dev transmission libxfce4ui-utils thunar xfce4 xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfwm4 firefox-esr fonts-noto fonts-noto-cjk fonts-noto-color-emoji arc-theme xserver-xorg xinit mpv renameutils apt-utils ca-certificates dirmngr software-properties-common apt-transport-https imv tldr podman -y
+
+# workspace
+mkdir ¬/zero
+mkdir ¬/Downloads
+
+# install nix pkg manager
+sh <(curl -L https://nixos.org/nix/install) --daemon
+
+echo "reboot for better experience"
