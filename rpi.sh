932181ffea1622cf6dff31121aec9bee831fa987
gobikrishnan-s3141 <139465475+gobikrishnan-s3141@users.noreply.github.com>
2024-12-19 16:28:51 +0000

    Update rpi.sh

--- rpi.sh
+++ rpi.sh
@@ -2,7 +2,7 @@
 
 # install packages
 sudo apt update
-sudo apt install build-essential bash-completion htop neofetch iftop udiskie rsync ssh tree git python3-dev cmake libssl-dev libxml2-dev libcurl4-openssl-dev libfontconfig-dev transmission libxfce4ui-utils thunar xfce4 xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfwm4 firefox-esr fonts-noto fonts-noto-cjk fonts-noto-color-emoji arc-theme xserver-xorg xinit mpv renameutils apt-utils ca-certificates dirmngr software-properties-common apt-transport-https imv tldr podman -y
+sudo apt install build-essential bash-completion htop neofetch iftop udiskie rsync ssh tree git python3-dev cmake libssl-dev libxml2-dev libcurl4-openssl-dev libfontconfig-dev transmission libxfce4ui-utils thunar xfce4 xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfwm4 firefox-esr fonts-noto fonts-noto-cjk fonts-noto-color-emoji arc-theme xserver-xorg xinit mpv renameutils apt-utils ca-certificates dirmngr software-properties-common apt-transport-https imv tldr ntp podman -y
 
 # workspace
 mkdir ¬/zero
